3ea48da35f7e5892543a82626b8458ac666a07b0
Mesteche <meztchek@gmail.com>
2019-10-09 13:07:02 +0000

    init: create initial component

---
+++ src/Atoms/Tooltip/Tooltip.test.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+// import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+// import userEvent from '@testing-library/user-event'
+
+// import Tooltip from './index'
+
+// TODO: implement test for Tooltip
+it.todo('Tooltip should be tested')
