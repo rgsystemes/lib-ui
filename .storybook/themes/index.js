8d62b06a42267a3771b1ede6cc566090a065b0b3
Mesteche <meztchek@gmail.com>
2019-10-09 12:40:54 +0000

    feat: implement banner

--- .storybook/themes/index.js
+++ .storybook/themes/index.js
@@ -1,36 +1,36 @@
 import * as rg6Theme from './rg6-theme'
 import * as darkTheme from './dark-theme'
 
-const space = [0, 4, 8, 16, 32].map(value => `${value}px`)
-space.s =  space[1]
-space.m =  space[2]
-space.l =  space[3]
-space.xl = space[4]
+const space    = [0, 4, 8, 16, 32].map(value => `${value}px`)
+      space.s  = space[1]
+      space.m  = space[2]
+      space.l  = space[3]
+      space.xl = space[4]
 
-const lineHeights = [0.25, 1, 1.25, 1.75]
-lineHeights.heading = lineHeights[2]
-lineHeights.body =    lineHeights[3]
+const lineHeights         = [0.25, 1, 1.25, 1.75]
+      lineHeights.heading = lineHeights[2]
+      lineHeights.body    = lineHeights[3]
 
 const levels = {
   error  : {
-    background : 'rgba(169, 67, 66, 0.25)',
+    background : '#f2dede',
     color      : '#a94442',
-    borderColor: '#a94442'
+    borderColor: '#ebccd1'
   },
   warning: {
-    background : 'rgba(139, 110, 60, 0.25)',
+    background : '#fcf8e3',
     color      : '#8a6d3b',
-    borderColor: '#8a6d3b',
+    borderColor: '#faebcc',
   },
   success: {
-    background : 'rgba(60, 118, 61, 0.25)',
+    background : '#dff0d8',
     color      : '#3c763d',
-    borderColor: '#3c763d',
+    borderColor: '#d6e9c6',
   },
   info   : {
-    background : 'rgba(49, 112, 143, 0.25)',
+    background : '#d9edf7',
     color      : '#31708f',
-    borderColor: '#31708f',
+    borderColor: '#bce8f1',
   },
 }
 
@@ -47,7 +47,7 @@ const base = {
   levels,
   radii: [0, 4],
   fontSizes,
-  fonts:            {
+  fonts: {
     body:      '"Helvetica Neue", Helvetica, Arial, sans-serif',
     monospace: 'monospace',
   },
@@ -57,12 +57,5 @@ const base = {
   }
 }
 
-export const rg6 = {
-  ...base,
-  ...rg6Theme,
-}
-
-export const dark = {
-  ...base,
-  ...darkTheme,
-}
+export const rg6 = { ...base, ...rg6Theme }
+export const dark = { ...base, ...darkTheme }
