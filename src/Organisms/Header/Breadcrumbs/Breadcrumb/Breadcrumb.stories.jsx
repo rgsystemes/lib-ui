670b543394494ef512de95f832a221cf3431167f
Maxime Bertrand <maxi.bertrand@gmail.com>
2019-10-17 11:01:07 +0200

    added Breadcrumb component

---
+++ src/Organisms/Header/Breadcrumbs/Breadcrumb/Breadcrumb.stories.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { text, number } from '@storybook/addon-knobs'
+
+import Breadcrumb from './index'
+
+import markdown from './README.md'
+
+export default {
+  title: 'Organisms/Header/Breadcrumbs/Breadcrumb',
+}
+
+const siblings = [
+  { id: 9302, name: 'Node 1' },
+  { id: 4302, name: 'Node 2' },
+  { id: 8302, name: 'Node 3' },
+]
+
+export const breadcrumb = () => (
+  <Breadcrumb
+    siblings={siblings}
+    onSiblingSelect={action('sibling selected')}
+  >
+    { text('Title', 'Node 24') }
+  </Breadcrumb>
+)
+
+breadcrumb.story = {
+  parameters: {
+    notes: { markdown },
+    // jest:  ['Breadcrumb.test.jsx'],
+  },
+}
