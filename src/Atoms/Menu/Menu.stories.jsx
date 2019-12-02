f3a6e718dceb6fbdba31fbb255b21287097eb2d0
Maxime Bertrand <maxi.bertrand@gmail.com>
2019-12-02 15:30:27 +0100

    fix: run eslint

--- src/Atoms/Menu/Menu.stories.jsx
+++ src/Atoms/Menu/Menu.stories.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { action } from '@storybook/addon-actions'
 import styled from 'styled-components'
 import { boolean } from '@storybook/addon-knobs'
 
