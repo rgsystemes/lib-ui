c712dccbdfc3f76b446274db5cdfeae80062590b
Maxime Bertrand <maxi.bertrand@gmail.com>
2019-12-05 15:17:45 +0100

    fix: delete IconButton test file

--- src/Atoms/IconButton/IconButton.stories.jsx
+++ src/Atoms/IconButton/IconButton.stories.jsx
@@ -24,6 +24,6 @@ export const iconButton = () => (
 iconButton.story = {
   parameters: {
     notes: { markdown },
-    jest:  ['IconButton.test.jsx'],
+    jest:  [],
   },
 }
--- src/Atoms/IconButton/IconButton.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import '@testing-library/jest-dom/extend-expect'
-import userEvent from '@testing-library/user-event'
-
-import IconButton from './index'
-
-// TODO: implement test for IconButton
-it.todo('IconButton should be tested')
