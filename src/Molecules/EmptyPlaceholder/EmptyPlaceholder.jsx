1534c86100fecc2e575aed9c5c6686ede6b52b28
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-01-31 11:07:44 +0100

    feat: add EmptyPlaceholder component

---
+++ src/Molecules/EmptyPlaceholder/EmptyPlaceholder.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { css } from '@styled-system/css'
+import { layout } from 'styled-system'
+import styled from 'styled-components'
+
+import Typo from '../../Atoms/Typo'
+
+const Container = styled.div`
+  ${layout}
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  & > * {
+    ${css({ my: 'l' })}
+  }
+`
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const EmptyPlaceholder = ({
+  primaryText,
+  secondaryText,
+  icon = null,
+  action = null,
+  ...props
+}) =>
+  <Container {...props}>
+    {icon}
+    <TextContainer>
+      <Typo fontSize="xl">
+        {primaryText}
+      </Typo>
+      <Typo fontSize="s">
+        {secondaryText}
+      </Typo>
+    </TextContainer>
+    {action}
+  </Container>
+
+EmptyPlaceholder.defaultProps = {
+  minHeight: '700px',
+}
+
+export default EmptyPlaceholder
