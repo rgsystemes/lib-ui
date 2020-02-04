f6a34e97ebfdc5a82417c80bb54ba3ccf746843b
LordWeedlle <virgile.cabane@supinfo.com>
2020-02-04 15:14:42 +0100

    UT optimization

--- src/Molecules/Breadcrumb/Breadcrumb.test.jsx
+++ src/Molecules/Breadcrumb/Breadcrumb.test.jsx
@@ -5,22 +5,22 @@ import { ThemeProvider } from 'styled-components'
 
 import Breadcrumb from './Breadcrumb'
 
+const WrappedBreadcrumb = props => (
+  <ThemeProvider theme={{}}>
+    <Breadcrumb {...props} />
+  </ThemeProvider>
+)
+
 it('should display a breadcrumb', () => {
   const path = [
     { id: 1, name: 'root' },
     { id: 5, name: 'element' },
-  ]
+  ].map(props => ({
+    ...props,
+    href: false,
+  }))
 
-  const { queryByTestId } = render(
-    <ThemeProvider theme={{}}>
-      <Breadcrumb
-        path={path.map(props => ({
-          ...props,
-          href: false,
-        }))}
-      />
-    </ThemeProvider>
-  )
+  const { queryByTestId } = render(<WrappedBreadcrumb path={path}/>)
 
   expect(queryByTestId('typo-1')).not.toBe(null)
   expect(queryByTestId('typo-5')).not.toBe(null)
@@ -34,23 +34,13 @@ it('should display a breadcrumb with link', () => {
   const path = [
     { id: 1, name: 'root' },
     { id: 5, name: 'element' },
-  ]
+  ].map(props => ({
+    ...props,
+    href: props.id,
+  }))
 
-  const { queryByTestId } = render(
-    <ThemeProvider theme={{}}>
-      <Breadcrumb
-        path={path.map(props => ({
-          ...props,
-          href: props.id,
-        }))}
-      />
-    </ThemeProvider>
-  )
+  const { queryByTestId } = render(<WrappedBreadcrumb path={path} />)
 
-  expect(queryByTestId('typo-1')).not.toBe(null)
-  expect(queryByTestId('typo-5')).not.toBe(null)
-  expect(queryByTestId('typo-1')).toHaveTextContent('root')
-  expect(queryByTestId('typo-5')).toHaveTextContent('element')
   expect(queryByTestId('link-1')).not.toBe(null)
   expect(queryByTestId('link-5')).not.toBe(null)
   expect(queryByTestId('link-1')).toHaveTextContent('root')
@@ -61,23 +51,13 @@ it('should display a breadcrumb with link except the last one', () => {
   const path = [
     { id: 1, name: 'root' },
     { id: 5, name: 'element' },
-  ]
+  ].map((props, index) => ({
+    ...props,
+    href: id => index === (path.length - 1) ? false : id,
+  }))
 
-  const { queryByTestId } = render(
-    <ThemeProvider theme={{}}>
-      <Breadcrumb
-        path={path.map((props, index) => ({
-          ...props,
-          href: id => index === (path.length - 1) ? false : id,
-        }))}
-      />
-    </ThemeProvider>
-  )
+  const { queryByTestId } = render(<WrappedBreadcrumb path={path} />)
 
-  expect(queryByTestId('typo-1')).not.toBe(null)
-  expect(queryByTestId('typo-5')).not.toBe(null)
-  expect(queryByTestId('typo-1')).toHaveTextContent('root')
-  expect(queryByTestId('typo-5')).toHaveTextContent('element')
   expect(queryByTestId('link-1')).not.toBe(null)
   expect(queryByTestId('link-5')).toBe(null)
   expect(queryByTestId('link-1')).toHaveTextContent('root')
