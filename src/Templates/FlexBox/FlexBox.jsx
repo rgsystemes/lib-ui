3bae3f208f19861ff36ef0acc4935368cb4c6cd8
Mesteche <meztchek@gmail.com>
2020-02-13 16:02:42 +0000

    feat: add FlexBox template

---
+++ src/Templates/FlexBox/FlexBox.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import Box from '@material-ui/core/Box'
+import { useTheme } from '@material-ui/core'
+
+const GappedBox = styled(Box)`
+  & > *:not(:last-child) {
+    ${({ gap }) => gap}
+  }
+`
+
+const useGap = (gap, direction) => {
+  const theme = useTheme()
+  const key = direction === 'column' ? 'margin-bottom' : 'margin-right'
+  const value = theme.spacing(gap)
+  return `${key}: ${value}px;`
+}
+
+const FlexBox = ({ gap = 0, ...props }) => {
+  gap = useGap(gap, props.flexDirection)
+
+  return <GappedBox display='flex' gap={gap} {...props} />
+}
+
+export default FlexBox
---
+++ src/Templates/FlexBox/FlexBox.stories.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react'
+
+import FlexBox from './index'
+
+import markdown from './README.md'
+
+export default {
+  title: 'Templates/FlexBox',
+}
+
+export const flexBox = () => {
+  const [gap, setGap] = useState(2)
+  const [padding, setPadding] = useState(1)
+  const [direction, setDirection] = useState('column')
+
+  return (
+    <FlexBox fontFamily='fontFamily' flexDirection='column'>
+      <label>
+        Flex direction: {direction}
+        <input
+          type='checkbox'
+          checked={direction === 'column'}
+          onChange={() => setDirection(
+            direction === 'column' ? 'row' : 'column',
+          )}
+        />
+      </label>
+      <label>
+        Gap
+        <input
+          type='range'
+          min='0' max='10'
+          value={gap}
+          onChange={ev => setGap(ev.target.value)}
+        />
+      </label>
+      <label>
+        Padding
+        <input
+          type='range'
+          min='0' max='10'
+          value={padding}
+          onChange={ev => setPadding(Number(ev.target.value))}
+        />
+      </label>
+      <FlexBox flexDirection={direction} gap={gap} p={padding} border='1px solid black'>
+        <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+        <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+        <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+      </FlexBox>
+    </FlexBox>
+  )
+}
+
+export const LayoutExample = () => {
+  const [gap, setGap] = useState(2)
+  const [padding, setPadding] = useState(1)
+
+  return (
+    <FlexBox fontFamily='fontFamily' flexDirection='column'>
+      <label>Gap
+        <input
+          type='range'
+          min='0' max='10'
+          value={gap}
+          onChange={ev => setGap(ev.target.value)}
+        />
+      </label>
+      <label>Padding
+        <input
+          type='range'
+          min='0' max='10'
+          value={padding}
+          onChange={ev => setPadding(Number(ev.target.value))}
+        />
+      </label>
+      <FlexBox gap={gap} p={padding} border='1px solid black'>
+        <FlexBox gap={gap} flexDirection='column' minWidth={300} minHeight={200}>
+          <FlexBox minWidth={300} minHeight={300} bgcolor='grey.300'/>
+          <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+        </FlexBox>
+        <FlexBox gap={gap} flexDirection='column' minWidth={300} minHeight={200}>
+          <FlexBox gap={gap} minWidth={300} minHeight={200}>
+            <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+            <FlexBox minWidth={300} minHeight={200} bgcolor='grey.300'/>
+          </FlexBox>
+          <FlexBox flexGrow={1} minWidth={300} minHeight={200} bgcolor='grey.300'/>
+        </FlexBox>
+      </FlexBox>
+    </FlexBox>
+  )
+}
+
+flexBox.story = {
+  parameters: {
+    notes: { markdown },
+    jest:  ['FlexBox.test.jsx'],
+  },
+}
