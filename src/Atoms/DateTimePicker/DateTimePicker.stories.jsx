219f34b91fed848d902a298d99b9b0c0e6c50fa0
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-02-17 15:31:52 +0100

    fix: remove useless import in stories

--- src/Atoms/DateTimePicker/DateTimePicker.stories.jsx
+++ src/Atoms/DateTimePicker/DateTimePicker.stories.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import {  } from '@storybook/addon-knobs'
 
 import DateTimePicker from './index'
 
--- src/Molecules/Filter/Filter.stories.jsx
+++ src/Molecules/Filter/Filter.stories.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { action } from '@storybook/addon-actions'
-import {  } from '@storybook/addon-knobs'
 import styled from 'styled-components'
 
 import Filter from './index'
