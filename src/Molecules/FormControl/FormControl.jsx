531da2eaf1638afe0961fece83eb8b392029abba
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-02-19 16:37:24 +0100

    fix: add width to FormControl

--- src/Molecules/FormControl/FormControl.jsx
+++ src/Molecules/FormControl/FormControl.jsx
@@ -3,6 +3,8 @@ import { css as systemCss } from '@styled-system/css'
 import BaseFormControl from '@material-ui/core/FormControl'
 
 export const formControlStyles = css`
+  width: 100%;
+  box-sizing: border-box;
   &.MuiFormControl-root:not(:first-child) {
     margin-top: 15px;
   }
