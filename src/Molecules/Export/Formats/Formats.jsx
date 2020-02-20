28374ff39d4392fd910f833790e867f2e5ba4284
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-02-20 16:26:19 +0100

    fix: remove useless useTranslation

--- src/Molecules/Export/Formats/Formats.jsx
+++ src/Molecules/Export/Formats/Formats.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 
-import Trans, { useTranslation } from '../../../Atoms/Trans'
+import Trans from '../../../Atoms/Trans'
 import FormControl from '../../../Molecules/FormControl'
 import InputLabel from '../../../Molecules/FormControl/InputLabel'
 import Select from '../../../Atoms/Select'
 
-const Formats = ({ formats, value, onChange }) => {
-  const t = useTranslation()
-
-  return formats.length > 0 &&
-    <FormControl>
-      <InputLabel>
-        <Trans transKey="global.export.format.label"/>
-        <Select value={value} onChange={event => onChange(event.target.value)}>
-          {formats.map(({ value, label }) => <option value={value} key={value}>{label}</option>)}
-        </Select>
-      </InputLabel>
-    </FormControl>
-}
+const Formats = ({ formats, value, onChange }) =>
+  formats.length > 0 &&
+  <FormControl>
+    <InputLabel>
+      <Trans transKey="global.export.format.label"/>
+      <Select value={value} onChange={event => onChange(event.target.value)}>
+        {formats.map(({ value, label }) => <option value={value} key={value}>{label}</option>)}
+      </Select>
+    </InputLabel>
+  </FormControl>
 
 export default Formats
