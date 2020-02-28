2c3934f7229b93a143528c7bb62bade9a4c1e383
Mesteche <meztchek@gmail.com>
2020-02-28 10:17:37 +0000

    feat: make translations easily overridable

--- src/Atoms/Trans/useTranslation.js
+++ src/Atoms/Trans/useTranslation.js
@@ -1,11 +1,23 @@
-import { createContext, useContext } from 'react'
+import React, { createContext, useContext, useMemo } from 'react'
+import { deepMerge } from '../../utils'
 const Context = createContext()
-export const { Provider } = Context
+const { Provider: BaseProvider } = Context
+
+export const Provider = ({ value, ...props }) => {
+  const translations = useContext(Context) || {}
+  const merged = useMemo(
+    () => deepMerge(translations, value),
+    [translations, value],
+  )
+
+  return <BaseProvider value={merged} {...props} />
+}
 
 export default () => {
   const translations = useContext(Context) || {}
 
   return (transKey = '', parameters = {}) => {
+    console.info(transKey.split('.'), translations)
     let translated = transKey
       .split('.')
       .reduce(
