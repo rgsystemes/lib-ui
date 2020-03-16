35dda780cc77e4e7b6859da38248142480375451
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-03-16 16:42:51 +0100

    feat: add more Editable examples of use

--- src/Molecules/Editable/Editable.stories.jsx
+++ src/Molecules/Editable/Editable.stories.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
-import { boolean } from '@storybook/addon-knobs'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import FormGroup from '@material-ui/core/FormGroup'
 import { format } from 'date-fns'
+import { Edit } from '@styled-icons/material/Edit'
+import { Check } from '@styled-icons/material/Check'
+import Box from '@material-ui/core/Box'
 
+import Button from '../../Atoms/Button'
+import IconButton from '../../Atoms/IconButton'
 import StatusChip from '../../Atoms/StatusChip'
 import FormControl, { FormControlLabel } from '../FormControl'
 import Editable from './index'
@@ -72,8 +76,8 @@ const CustomFormControl = ({
   </FormControl>
 }
 
-export const editable = () => {
-  const edit = boolean('Edit', false)
+export const groupedEditable = () => {
+  const [edit, setEdit] = useState(false)
   const [value, baseSetValue] = useState({
     text:     'Text',
     select:   'choice3',
@@ -89,83 +93,122 @@ export const editable = () => {
   const [customFirst, customSecond] = custom
 
   return (
-    <FlexBox gap={2} flexDirection="column">
+    <>
+      <FlexBox gap={2} flexDirection="column">
+        <Editable
+          edit={edit}
+          value={text}
+          label="Label"
+          onChange={e => setValue({ ...value, text: e.target.value })}
+        >
+          <Typo>
+            {text}
+          </Typo>
+        </Editable>
+        <Editable
+          edit={edit}
+          Type={EditableSelect}
+          value={select}
+          label="Label"
+          onChange={e => setValue({ select: e.target.value })}
+        >
+          <Typo>
+            {select}
+          </Typo>
+        </Editable>
+        <Editable
+          edit={edit}
+          Type={EditableRadioGroup}
+          value={select}
+          label="Label"
+          onChange={e => setValue({ select: e.target.value })}
+        >
+          <Typo>
+            {select}
+          </Typo>
+        </Editable>
+        <Editable
+          edit={edit}
+          Type={EditableCheckboxGroup}
+          checked={checkbox}
+          label="Label"
+          onChange={(name, value) => setValue({ checkbox: { ...checkbox, [name]: value } })}
+        >
+          <FlexBox as={Typo} gap={2}>
+            {Object
+              .entries(checkbox)
+              .filter(([name, value]) => !!value)
+              .map(([name, value]) =>
+                <StatusChip>
+                  {name}
+                </StatusChip>,
+              )
+            }
+          </FlexBox>
+        </Editable>
+        <Editable
+          edit={edit}
+          Type={DateTimePicker}
+          value={date}
+          label="Label"
+          onChange={date => setValue({ date })}
+        >
+          <Typo>
+            {format(date, 'Pp')}
+          </Typo>
+        </Editable>
+        <Editable
+          edit={edit}
+          FormControl={CustomFormControl}
+          label="Label"
+          value={custom}
+          onChange={custom => setValue({ custom })}
+        >
+          <Typo>The first value is : {customFirst}</Typo>
+          <Typo>The first value is : {customSecond}</Typo>
+        </Editable>
+      </FlexBox>
+      <Box mt={2}>
+        <Button onClick={() => setEdit(!edit)}>
+          {edit ? 'Submit' : 'Edit'}
+        </Button>
+      </Box>
+    </>
+  )
+}
+
+groupedEditable.story = {
+  parameters: {
+    notes: { markdown },
+    jest:  ['Editable.test.jsx'],
+  },
+}
+
+export const standaloneEditable = () => {
+  const [value, setValue] = useState('Obi-wan Kenobi')
+  const [edit, setEdit] = useState(false)
+
+  return (
+    <FlexBox gap={0.5}>
       <Editable
         edit={edit}
-        value={text}
-        label="Label"
-        onChange={e => setValue({ ...value, text: e.target.value })}
+        label={false}
+        value={value}
+        size="small"
+        onChange={e => setValue(e.target.value)}
       >
-        <Typo>
-          {text}
+        <Typo onClick={() => setEdit(true)}>
+          {value}
         </Typo>
       </Editable>
-      <Editable
-        edit={edit}
-        Type={EditableSelect}
-        value={select}
-        label="Label"
-        onChange={e => setValue({ select: e.target.value })}
-      >
-        <Typo>
-          {select}
-        </Typo>
-      </Editable>
-      <Editable
-        edit={edit}
-        Type={EditableRadioGroup}
-        value={select}
-        label="Label"
-        onChange={e => setValue({ select: e.target.value })}
-      >
-        <Typo>
-          {select}
-        </Typo>
-      </Editable>
-      <Editable
-        edit={edit}
-        Type={EditableCheckboxGroup}
-        checked={checkbox}
-        label="Label"
-        onChange={(name, value) => setValue({ checkbox: { ...checkbox, [name]: value } })}
-      >
-        <FlexBox as={Typo} gap={2}>
-          {Object
-            .entries(checkbox)
-            .filter(([name, value]) => !!value)
-            .map(([name, value]) =>
-              <StatusChip>
-                {name}
-              </StatusChip>,
-            )
-          }
-        </FlexBox>
-      </Editable>
-      <Editable
-        edit={edit}
-        Type={DateTimePicker}
-        value={date}
-        label="Label"
-        onChange={date => setValue({ date })}
-      >
-        <Typo>
-          {format(date, 'Pp')}
-        </Typo>
-      </Editable>
-      <Editable
-        edit={edit}
-        FormControl={CustomFormControl}
-        label="Label"
-        value={custom}
-        onChange={custom => setValue({ custom })}
-      >
-        <Typo>The first value is : {customFirst}</Typo>
-        <Typo>The first value is : {customSecond}</Typo>
-      </Editable>
+      <IconButton size="small" onClick={() => setEdit(!edit)}>
+        {edit ? <Check size={16}/> : <Edit size={16} />}
+      </IconButton>
     </FlexBox>
   )
 }
-editable.story = {
+
+standaloneEditable.story = {
   parameters: {
     notes: { markdown },
     jest:  ['Editable.test.jsx'],
