cf01aa9366e10abdf41d29b11b07e455cbe6a7f0
Maxime Bertrand <maxi.bertrand@gmail.com>
2020-03-16 15:40:35 +0100

    feat: implement Editable component

---
+++ src/Molecules/Editable/Editable.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import FormControl from '../FormControl'
+import FormHelperText from '../FormControl/FormHelperText'
+import InputLabel from '../FormControl/InputLabel'
+import Input from '../../Atoms/Input'
+import Typo from '../../Atoms/Typo'
+import FlexBox from '../../Templates/FlexBox'
+
+const DefaulFromControl = ({
+  Type = Input,
+  label,
+  helpText = '',
+  ...props
+}) =>
+  <FormControl>
+    <InputLabel>
+      {label}
+      <Type {...props} />
+    </InputLabel>
+    <FormHelperText>
+      {helpText}
+    </FormHelperText>
+  </FormControl>
+
+const Editable = ({
+  children,
+  edit,
+  onEdit = () => {},
+  onValid = () => {},
+  FormControl = DefaulFromControl,
+  label,
+  ...props
+}) => (
+  edit ? <FormControl label={label} {...props}/> : (
+    <FlexBox onClick={() => onEdit(true)} gap={1} flexDirection="column">
+      <Typo fontSize="title" fontWeight="title" fontFamily="title">
+        {label}
+      </Typo>
+      {children}
+    </FlexBox>
+  )
+)
+
+export default Editable
---
+++ src/Molecules/Editable/Editable.stories.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from 'react'
+import { action } from '@storybook/addon-actions'
+import { boolean } from '@storybook/addon-knobs'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import FormGroup from '@material-ui/core/FormGroup'
+import { format } from 'date-fns'
+
+import StatusChip from '../../Atoms/StatusChip'
+import FormControl, { FormControlLabel } from '../FormControl'
+import Editable from './index'
+import FlexBox from '../../Templates/FlexBox'
+import Select from '../../Atoms/Select'
+import Typo from '../../Atoms/Typo'
+import Checkbox from '../../Atoms/Checkbox'
+import DateTimePicker from '../../Atoms/DateTimePicker'
+import Radio from '../../Atoms/Radio'
+
+import markdown from './README.md'
+
+export default {
+  title: 'Molecules/Editable',
+}
+
+const options = [
+  { value: 'choice1', label: 'Choice 1' },
+  { value: 'choice2', label: 'Choice 2' },
+  { value: 'choice3', label: 'Choice 3' },
+  { value: 'choice4', label: 'Choice 4' },
+]
+
+const EditableSelect = ({ ...props }) =>
+  <Select {...props}>
+    {options.map(({ value, label }) =>
+      <option value={value}>{label}</option>,
+    )}
+  </Select>
+
+const EditableCheckboxGroup = ({ checked, onChange, ...props }) =>
+  <FormGroup aria-label="checkboxGroup" name="checkboxGroup" {...props}>
+    {options.map(({ value, label }) =>
+      <FormControlLabel
+        control={
+          <Checkbox
+            value={value}
+            checked={checked[value]}
+            onChange={(e, checked) => onChange(value, checked)}
+          />
+        }
+        label={label}
+      />,
+    )}
+  </FormGroup>
+
+const EditableRadioGroup = ({ ...props }) =>
+  <RadioGroup aria-label="radioGroup" name="radioGroup" {...props}>
+    {options.map(({ value, label }) =>
+      <FormControlLabel value={value} control={<Radio />} label={label} />,
+    )}
+  </RadioGroup>
+
+const CustomFormControl = ({
+  value = [],
+  onChange = () => {},
+  ...props
+}) => {
+  const [first, second] = value
+
+  return <FormControl {...props}>
+    <FlexBox gap={1}>
+      <EditableSelect value={first} onChange={e => onChange([e.target.value, second])}/>
+      <EditableSelect value={second} onChange={e => onChange([first, e.target.value])}/>
+    </FlexBox>
+  </FormControl>
+}
+
+export const editable = () => {
+  const edit = boolean('Edit', false)
+  const [value, baseSetValue] = useState({
+    text:     'Text',
+    select:   'choice3',
+    date:     new Date(),
+    checkbox: { choice4: true },
+    custom:   ['choice1', 'choice3'],
+  })
+
+  const setValue = newValue => baseSetValue({ ...value, ...newValue })
+  const {
+    text, select, checkbox, date, custom = [],
+  } = value
+  const [customFirst, customSecond] = custom
+
+  return (
+    <FlexBox gap={2} flexDirection="column">
+      <Editable
+        edit={edit}
+        value={text}
+        label="Label"
+        onChange={e => setValue({ ...value, text: e.target.value })}
+      >
+        <Typo>
+          {text}
+        </Typo>
+      </Editable>
+      <Editable
+        edit={edit}
+        Type={EditableSelect}
+        value={select}
+        label="Label"
+        onChange={e => setValue({ select: e.target.value })}
+      >
+        <Typo>
+          {select}
+        </Typo>
+      </Editable>
+      <Editable
+        edit={edit}
+        Type={EditableRadioGroup}
+        value={select}
+        label="Label"
+        onChange={e => setValue({ select: e.target.value })}
+      >
+        <Typo>
+          {select}
+        </Typo>
+      </Editable>
+      <Editable
+        edit={edit}
+        Type={EditableCheckboxGroup}
+        checked={checkbox}
+        label="Label"
+        onChange={(name, value) => setValue({ checkbox: { ...checkbox, [name]: value } })}
+      >
+        <FlexBox as={Typo} gap={2}>
+          {Object
+            .entries(checkbox)
+            .filter(([name, value]) => !!value)
+            .map(([name, value]) =>
+              <StatusChip>
+                {name}
+              </StatusChip>,
+            )
+          }
+        </FlexBox>
+      </Editable>
+      <Editable
+        edit={edit}
+        Type={DateTimePicker}
+        value={date}
+        label="Label"
+        onChange={date => setValue({ date })}
+      >
+        <Typo>
+          {format(date, 'Pp')}
+        </Typo>
+      </Editable>
+      <Editable
+        edit={edit}
+        FormControl={CustomFormControl}
+        label="Label"
+        value={custom}
+        onChange={custom => setValue({ custom })}
+      >
+        <Typo>The first value is : {customFirst}</Typo>
+        <Typo>The first value is : {customSecond}</Typo>
+      </Editable>
+    </FlexBox>
+  )
+}
+editable.story = {
+  parameters: {
+    notes: { markdown },
+    jest:  ['Editable.test.jsx'],
+  },
+}
