bc41bec1396c6da177959e76749aa6ddadd54155
Virgile Cabane <virgile.cabane@supinfo.com>
2020-03-25 15:59:23 +0100

    [REVIEW] Add Card component (#72)
    * Add Card component
    
    * Stop breaking my project plz
    
    * @mesteche review
    
    Co-Authored-By: Mesteche <meztchek@gmail.com>
    
    * Make sure @Skalina is happy
    
    Co-authored-by: Mesteche <meztchek@gmail.com>

---
+++ src/Atoms/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import BaseCard from '@material-ui/core/Card'
+
+import { FlexBox } from '../../Templates'
+
+const Card = props => <FlexBox
+  component={BaseCard}
+  gap={2}
+  p={2}
+  bgcolor="grey.100"
+  borderRadius="borderRadius"
+  flexDirection="column"
+  boxShadow={0}
+  {...props}
+/>
+
+export default Card
