b6fb16bee32ac22acf97a0bde0df63f14efa1ac1
Beny13 <benoit.boivin@rgsystem.com>
2020-04-21 11:54:47 +0200

    Used themes

--- src/Atoms/Switch/Switch.jsx
+++ src/Atoms/Switch/Switch.jsx
@@ -2,211 +2,218 @@ import React, { useState } from 'react'
 import classNames from 'classnames'
 import { makeStyles } from '@material-ui/core/styles'
 
-const variants = {
-  primary: {
-    background: '#009cb4',
-    color: '#f5f5f5',
-    hover: '#00859a',
-    borderColor: '#009cb4',
-    disabled: {
-      background: '#59bfce',
-      color: '#c5e9ee',
+const useStyles = makeStyles(theme => {
+  const variants = {
+    primary: {
+      background: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+      hover: theme.palette.primary.dark,
+      borderColor: theme.palette.primary.main,
+      disabled: {
+        background: theme.palette.primary.light,
+        color: theme.palette.primary.contrastText,
+      },
     },
-  },
-  success: {
-    background: '#5CB85C',
-    color: '#f5f5f5',
-    hover: '#449d44',
-    borderColor: '#5CB85C',
-    disabled: {
-      background: '#95d195',
-      color: '#daefda',
+    success: {
+      background: theme.palette.success.main,
+      color: theme.palette.success.contrastText,
+      hover: theme.palette.success.dark,
+      borderColor: theme.palette.success.main,
+      disabled: {
+        background: theme.palette.success.light,
+        color: theme.palette.success.contrastText,
+      },
     },
-  },
-  default: {
-    background: 'white',
-    color: 'black',
-    hover: '#e6e6e6',
-    borderColor: '#ccc',
-    disabled: {
-      background: '#e6e6e6',
-      color: '#7a7a7a',
+    default: {
+      background: 'white',
+      color: 'black',
+      hover: '#e6e6e6',
+      borderColor: '#ccc',
+      disabled: {
+        background: '#e6e6e6',
+        color: '#7a7a7a',
+      },
     },
-  },
-}
+  }
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    fontFamily: 'sans-serif',
-    width: '57px',
-    height: '32px',
-    display: 'inline-block',
-    marginBottom: 0,
-    fontSize: '14px',
-    fontWeight: 400,
-    lineHeight: 1.5,
-    textAlign: 'center',
-    whiteSpace: 'nowrap',
-    verticalAlign: 'middle',
-    cursor: 'pointer',
-    border: '1px solid #adadad',
-    borderRadius: '4px',
-    position: 'relative',
-    overflow: 'hidden',
-    '& .button': {
+  return {
+    root: {
+      fontFamily: 'sans-serif',
+      width: '57px',
+      height: '32px',
       display: 'inline-block',
-      padding: '6px 12px',
       marginBottom: 0,
+      fontSize: '14px',
+      fontWeight: 400,
+      lineHeight: 1.5,
       textAlign: 'center',
       whiteSpace: 'nowrap',
+      verticalAlign: 'middle',
       cursor: 'pointer',
-      userSelect: 'none',
-      position: 'absolute',
-      margin: 0,
-      top: 0,
-    },
-    '&:not(.switch-checked) .switch-group': {
-      left: '-100%',
-    },
-    '& > .switch-group': {
-      textAlign: 'center',
-      whiteSpace: 'nowrap',
-      cursor: 'pointer',
-      position: 'absolute',
-      width: '200%',
-      top: 0,
-      bottom: 0,
-      left: 0,
-      transition: 'left 0.35s',
-      '& > .switch-on': {
+      border: '1px solid #ccc',
+      borderRadius: '4px',
+      position: 'relative',
+      overflow: 'hidden',
+      '& .button': {
+        display: 'inline-block',
+        padding: '6px 12px',
+        marginBottom: 0,
+        textAlign: 'center',
+        whiteSpace: 'nowrap',
+        cursor: 'pointer',
+        userSelect: 'none',
+        position: 'absolute',
+        margin: 0,
+        top: 0,
+      },
+      '&:not(.switch-checked) .switch-group': {
+        left: '-100%',
+      },
+      '& > .switch-group': {
+        textAlign: 'center',
+        whiteSpace: 'nowrap',
+        cursor: 'pointer',
+        position: 'absolute',
+        width: '200%',
+        top: 0,
+        bottom: 0,
         left: 0,
-        paddingRight: '24px',
-        right: '50%',
-      },
-      '& > .switch-off': {
-        left: '50%',
-        paddingLeft: '24px',
-        right: 0,
-      },
-      '& > .switch-slider': {
-        backgroundColor: '#fff',
-        position: 'relative',
-        margin: '0 auto',
-        paddingTop: '0px',
-        paddingBottom: '0px',
-        height: '100%',
-        width: '4px',
-        borderRadius: '4px',
-        borderWidth: '0px 1px',
-        borderColor: '#ccc',
-        borderStyle: 'solid',
-      },
-    },
-    // Sizes
-    '&.switch-small': {
-      width: '53px',
-      height: '28px',
-      '& > .switch-group': {
-        '& .switch-on': {
-          paddingTop: '4px',
-          paddingLeft: '11px',
+        transition: 'left 0.35s',
+        '& > .switch-on': {
+          left: 0,
+          paddingRight: '24px',
+          right: '50%',
         },
-        '& .switch-off': {
-          paddingTop: '4px',
-          paddingLeft: '23px',
+        '& > .switch-off': {
+          left: '50%',
+          paddingLeft: '24px',
+          right: 0,
+          backgroundColor: '#e6e6e6',
         },
-        '& .switch-slider': {
-          width: '2px',
-          padding: '0px 10px',
+        '& > .switch-slider': {
+          backgroundColor: '#fff',
+          position: 'relative',
+          margin: '0 auto',
+          paddingTop: '0px',
+          paddingBottom: '0px',
+          height: '100%',
+          width: '4px',
+          borderRadius: '4px',
+          borderWidth: '0px 1px',
+          borderColor: '#ccc',
+          borderStyle: 'solid',
         },
       },
-    },
-    '&.switch-smallest': {
-      width: '34px',
-      height: '20px',
-      fontSize: '11px',
-      '& > .switch-group': {
-        '& .switch-on': {
-          paddingTop: '2px',
-          paddingLeft: '7px',
-        },
-        '& .switch-off': {
-          paddingTop: '2px',
-          paddingLeft: '12px',
-        },
-        '& .switch-slider': {
-          width: '1px',
-          padding: '0px 5px',
+      // Sizes
+      '&.switch-small': {
+        width: '53px',
+        height: '28px',
+        '& > .switch-group': {
+          '& .switch-on': {
+            paddingTop: '4px',
+            paddingLeft: '11px',
+          },
+          '& .switch-off': {
+            paddingTop: '4px',
+            paddingLeft: '23px',
+          },
+          '& .switch-slider': {
+            width: '2px',
+            padding: '0px 10px',
+          },
         },
       },
-    },
-    // Disabled
-    '&.disabled': {
-      cursor: 'not-allowed',
-      '*': {
+      '&.switch-smallest': {
+        width: '34px',
+        height: '20px',
+        fontSize: '11px',
+        '& > .switch-group': {
+          '& .switch-on': {
+            paddingTop: '2px',
+            paddingLeft: '7px',
+          },
+          '& .switch-off': {
+            paddingTop: '2px',
+            paddingLeft: '12px',
+          },
+          '& .switch-slider': {
+            width: '1px',
+            padding: '0px 5px',
+          },
+        },
+      },
+      // Disabled
+      '&.disabled': {
+        borderColor: '#dedede',
         cursor: 'not-allowed',
-      }
-    },
-    // Color stuff
-    // TODO use theme
-    // TODO cool method to generate these?
-    // TODO fade et darken
-    '&.switch-color-primary': {
-      '&.switch-checked': {
-        borderColor: variants.primary.borderColor,
+        '*': {
+          cursor: 'not-allowed',
+        },
+        '& > .switch-group': {
+          '& > .switch-off': {
+            backgroundColor: '#efefef',
+            color: '#747474',
+          },
+        },
       },
-      '& .switch-on': {
-        backgroundColor: variants.primary.background,
-        color: variants.primary.color,
-      },
-      '&.disabled': {
+      // Color stuff
+      '&.switch-color-primary': {
         '&.switch-checked': {
-          borderColor: variants.primary.disabled.background,
+          borderColor: variants.primary.borderColor,
         },
         '& .switch-on': {
-          backgroundColor: variants.primary.disabled.background,
-          color: variants.primary.disabled.color,
-        }
+          backgroundColor: variants.primary.background,
+          color: variants.primary.color,
+        },
+        '&.disabled': {
+          '&.switch-checked': {
+            borderColor: variants.primary.disabled.background,
+          },
+          '& .switch-on': {
+            backgroundColor: variants.primary.disabled.background,
+            color: variants.primary.disabled.color,
+          }
+        },
       },
-    },
-    '&.switch-color-success': {
-      '&.switch-checked': {
-        borderColor: variants.success.borderColor,
-      },
-      '& .switch-on': {
-        backgroundColor: variants.success.background,
-        color: variants.success.color,
-      },
-      '&.disabled': {
+      '&.switch-color-success': {
         '&.switch-checked': {
-          borderColor: variants.success.disabled.background,
+          borderColor: variants.success.borderColor,
         },
         '& .switch-on': {
-          backgroundColor: variants.success.disabled.background,
-          color: variants.success.disabled.color,
-        }
+          backgroundColor: variants.success.background,
+          color: variants.success.color,
+        },
+        '&.disabled': {
+          '&.switch-checked': {
+            borderColor: variants.success.disabled.background,
+          },
+          '& .switch-on': {
+            backgroundColor: variants.success.disabled.background,
+            color: variants.success.disabled.color,
+          }
+        },
       },
-    },
-    '&.switch-color-default': {
-      '&.switch-checked': {
-        borderColor: variants.default.borderColor,
-      },
-      '& .switch-on': {
-        backgroundColor: variants.default.background,
-        color: variants.default.color,
-      },
-      '&.disabled': {
+      '&.switch-color-default': {
         '&.switch-checked': {
-          borderColor: variants.default.disabled.background,
+          borderColor: variants.default.borderColor,
         },
         '& .switch-on': {
-          backgroundColor: variants.default.disabled.background,
-          color: variants.default.disabled.color,
-        }
+          backgroundColor: variants.default.background,
+          color: variants.default.color,
+        },
+        '&.disabled': {
+          '&.switch-checked': {
+            borderColor: variants.default.disabled.background,
+          },
+          '& .switch-on': {
+            backgroundColor: variants.default.disabled.background,
+            color: variants.default.disabled.color,
+          }
+        },
       },
     },
-  },
-}))
+  }
+})
 
 export const Switch = ({ className, disabled = false, onChange = () => {}, checked = false, size = 'medium', color = 'primary', ...props }) => {
   const [state, setState] = useState(checked)
@@ -238,8 +245,7 @@ export const Switch = ({ className, disabled = false, onChange = () => {}, check
   </div>
 }
 
-// TODO utiliser makestyles (à confirmer) makeStyles(theme => createStyles({
-// TODO utiliser large au lieu de smallest?
+// TODO utiliser large au lieu de smallest? > large
 // TODO TU lul
 
 export default Switch
