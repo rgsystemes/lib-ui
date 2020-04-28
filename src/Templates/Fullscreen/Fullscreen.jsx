d6d774f0871c6b68059933e08cf61ef9248834b6
Beny13 <benoit.boivin@rgsystem.com>
2020-04-28 10:08:33 +0200

    Added translations, dropped fade

--- src/Templates/Fullscreen/Fullscreen.jsx
+++ src/Templates/Fullscreen/Fullscreen.jsx
@@ -1,12 +1,11 @@
-// TODO translations
-// TODO fix anim
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { Modal, Fade } from '@material-ui/core'
-import { Button, IconButton, Tooltip, Typo } from '../../Atoms'
+import { Modal } from '@material-ui/core'
+import { Button, IconButton, Tooltip, Typo, Trans } from '../../Atoms'
 import { X } from 'styled-icons/feather/X'
 import { Question } from '@styled-icons/evil/Question'
 import FlexBox from '../FlexBox'
+import { useTranslation } from '../../Atoms/Trans'
 
 const useStyles = makeStyles(theme => {
   return {
@@ -62,37 +61,40 @@ const Fullscreen = ({
     handleClose()
   }
 
+  const t = useTranslation()
+  const transKeyCancel = 'global.action.cancel'
+
   return <Modal className={useStyles().root} open={open} {...props} hideBackdrop closeAfterTransition>
-    <Fade in={open}>
-      <>
-        <div className={'header'}>
-          <FlexBox className={'title'}>
-            {icon}
-            <Typo fontSize="xs" fontWeight="title" fontFamily="title">{headerTitle}</Typo>
-          </FlexBox>
-          <FlexBox className={'subTitle'}>
-            <Typo fontSize="l" fontFamily="title">{title}</Typo>
-            {tooltip &&
-              <Tooltip title={tooltip}>
-                <Question size={25} />
-              </Tooltip>
-            }
-          </FlexBox>
-        </div>
-        <div className={'body'}>
-          {children}
-        </div>
-        <IconButton className={'close'} onClick={handleClose}>
-          <X size={50} />
-        </IconButton>
-        <div className={'buttons'}>
-          <Button color={'default'} onClick={handleClose}>Nope</Button>
-          {!!validateText &&
-            <Button color={'success'} onClick={handleValidate}>{validateText}</Button>
+    <>
+      <div className={'header'}>
+        <FlexBox className={'title'}>
+          {icon}
+          <Typo fontSize="xs" fontWeight="title" fontFamily="title">{headerTitle}</Typo>
+        </FlexBox>
+        <FlexBox className={'subTitle'}>
+          <Typo fontSize="l" fontFamily="title">{title}</Typo>
+          {tooltip &&
+            <Tooltip title={tooltip} data-testid={'tooltip'}>
+              <Question size={25} />
+            </Tooltip>
           }
-        </div>
-      </>
-    </Fade>
+        </FlexBox>
+      </div>
+      <div className={'body'}>
+        {children}
+      </div>
+      <IconButton className={'close'} onClick={handleClose} title={t(transKeyCancel)}>
+        <X size={50} />
+      </IconButton>
+      <div className={'buttons'}>
+        <Button color={'default'} onClick={handleClose}>
+          <Trans transKey={transKeyCancel} />
+        </Button>
+        {!!validateText &&
+          <Button color={'success'} onClick={handleValidate}>{validateText}</Button>
+        }
+      </div>
+    </>
   </Modal>
 }
 
